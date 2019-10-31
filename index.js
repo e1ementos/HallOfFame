8e21fdce93b37ab478e380fc209486af09ed6e3c
Asume <357noob@gmail.com>
2019-10-31 13:25:41 +0000

    t

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ break;
 case "wladd":
 
 if (args[1]) {
-        let name = client.users.find(user => user.username == args[1])
+        let name = guild.members.find(user => user.username == args[1])
         let id = name.id
         var user1 = args[1]
         wlmembers.push(user1);
